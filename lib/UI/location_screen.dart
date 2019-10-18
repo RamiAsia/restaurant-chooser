e95ac0eae3785c0390ec1e59c2b7cdbb935f6347
Rami Asia <rasia@proteus.com>
2019-10-18 13:48:33 -0700

    Added city finder

--- lib/UI/location_screen.dart
+++ lib/UI/location_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_finder/BLoC/bloc_provider.dart';
+import 'package:restaurant_finder/BLoC/location_bloc.dart';
 import 'package:restaurant_finder/BLoC/location_query_bloc.dart';
+import 'package:restaurant_finder/DataLayer/location.dart';
 
 class LocationScreen extends StatelessWidget {
+  final bool isFullScreenDialog;
+
+  LocationScreen({Key key, this.isFullScreenDialog = false}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final bloc = CityQueryBloc();
@@ -34,9 +40,45 @@ class LocationScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildResults() {
-    return Center(
-      child: Text("Enter a location"),
+  Widget _buildResults(CityQueryBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.cityStream,
+      builder: (context, snapshot) {
+        final results = snapshot.data;
+
+        if (results == null) {
+          return Container();
+        }
+
+        if (results.isEmpty) {
+          return Center(
+            child: Text("Sorry, no results"),
+          );
+        }
+
+        return _buildSearchResults(results);
+      },
+    );
+  }
+
+  Widget _buildSearchResults(List<City> results) {
+    return ListView.separated(
+      itemCount: results.length,
+      separatorBuilder: (BuildContext context, int index) => Divider(),
+      itemBuilder: (context, index) {
+        final city = results[index];
+        return ListTile(
+          title: Text(city.title),
+          onTap: () {
+            final cityBloc = BlocProvider.of<CityBloc>(context);
+            cityBloc.selectCity(city);
+
+            if (isFullScreenDialog) {
+              Navigator.of(context).pop();
+            }
+          },
+        );
+      },
     );
   }
 }
